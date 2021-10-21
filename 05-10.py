3715e33dd7e7e3c426c6dfd691b365822b17834c
AlanAjinjal <91607722+AlanAjinjal@users.noreply.github.com>
2021-10-21 17:01:23 +0300

    Update 05-10.py

--- 05-10.py
+++ 05-10.py
@@ -1,22 +1,13 @@
-def abb(name):
-    full = name.split()
-    if full[0][0].isupper and full[0][1:].islower and full[1][0].isupper and full[1][1:].islower and full[2][
-        0].isupper and full[2][1:].islower:
-        full[1] = full[1][0] + "."
-        full[2] = full[2][0] + "."
-        return ' '.join(full)
-    else:
-        return 'False'
+def gen_name(name, num):
+    i= 1
+    while i<num:
+        yield name 
+        i+=1
 
 
-def name_input():
-    list_of_names = []
-    while input() != "q":
-        b = [input(), input(), input()]
-        if abb(' '.join(b)):
-            list_of_names.append(abb(' '.join(b)))
-        else:
-            print("Error")
-            break
-    print(list_of_names)
-print(name_input())
+ranger= gen_name('Алан', 19)
+ranger2= gen_name('Максим', 18)
+names= [x for x in ranger]
+names2= [x for x in ranger2]
+print(names)
+print(names2)
